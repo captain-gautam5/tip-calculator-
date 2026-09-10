188664f3672a7a75f13e60988d7274929695e1b0
Gautam <gautamishra70@gmail.com>
2026-09-11 00:42:05 +0530

    add the split method

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import img from "../assets/frame.png"
 import download from "../assets/download.png"
+import user from "../assets/user.png"
 
 const Hero = () => {
     return (
-        <div className='h-[800px] w-full  mt-3 rounded-xl shadow-xl flex p-25'>
+        <div className='h-[800px] w-full  mt-3 rounded-xl shadow-xl flex justify-evenly p-25'>
 
             <div className='flex flex-col gap-5 rounded-3xl  p-5 w-[1000px]'>
                 {/* {about project} */}
@@ -31,8 +32,59 @@ const Hero = () => {
 
             </div>
 
-            <div>
+            <div className='w-200 h-150 rounded-[4rem] flex'>
                 {/* calculator */}
+
+                <div className='h-full w-2/3 rounded-l-4xl shadow-xl p-5 flex flex-col gap-5 '>
+                    {/* bill section */}
+
+                    <div className='flex flex-col gap-5 mb-3'>
+                        <h2 className='text-xl font-bold text-gray-400'>BILL TOTAL</h2>
+
+                        <input type="Number" placeholder='Enter your amount' className=' border-2 rounded-lg border-black/10 p-4 text-lg font-bold bg-gray-100 outline-none' />
+                    </div>
+
+                    <div className='flex flex-col gap-2 mb-6'>
+                        {/* tip selection */}
+                        <h2 className='text-xl font-bold text-gray-400'>SELECT TIP %</h2>
+
+                        <div className='flex gap-10 justify-evenly flex-wrap'>
+                            {/* tip amounts */}
+                            <button className='p-6 text-xl font-bold border border-black/10 rounded-xl hover:bg-gray-200 cursor-pointer w-1/4'>10%</button>
+                            <button className='p-6 text-xl font-bold border border-black/10 rounded-xl hover:bg-gray-200 cursor-pointer'>15%</button>
+                            <button className='p-6 text-xl font-bold border border-black/10 rounded-xl hover:bg-gray-200 cursor-pointer'>18%</button>
+                            <button className='p-6 text-xl font-bold border border-black/10 rounded-xl hover:bg-gray-200 cursor-pointer'>20%</button>
+                            <button className='p-6 text-xl font-bold border border-black/10 rounded-xl hover:bg-gray-200 cursor-pointer'>Custom</button>
+
+                        </div>
+
+
+                    </div>
+
+                    <div className='flex flex-col gap-2'>
+                        <h2 className='text-xl font-bold text-gray-400'>NUMBER OF PEOPLE</h2>
+
+                        <div className='flex p-4 justify-between w-full bg-gray-100 rounded-2xl items-center'>
+
+                            <button className='p-2 px-4 border border-black/10 font-bold text-[1.5rem] rounded-xl '>-</button>
+
+                            <div className='flex items-center gap-1'>
+                                <img src={user} className='w-8' alt="" /><div className='text-2xl font-bold'>1</div>
+                            </div>
+
+                            <button className='p-2 px-4 border border-black/10 font-bold text-[1.5rem] rounded-xl '>+</button>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+                <div className='bg-[#0D9488] w-1/3  rounded-r-4xl shadow-xl'>
+
+
+                </div>
 
             </div>
 
